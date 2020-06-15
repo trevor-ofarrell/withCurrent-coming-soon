249ccdd4a20719b707d04185763192ef9922687c
Trevor O'Farrell <trevor@fullsignal.co>
2020-06-15 12:04:48 -0700

    hiding api key

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 module.exports = {
   env: {
-    SENDGRID_API_KEY: "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"
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
   }
 }
